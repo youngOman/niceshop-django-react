b54eec8e734822904f6c9e16d89745cd7d653f92
young <loyang0921@gmail.com>
2025-07-27 17:58:30 +0800

    feat: 新增首頁圖

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -1,19 +1,18 @@
-
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const AdminRoute = ({ children }) => {
-    const navigate = useNavigate();
-    const { userInfo } = useSelector((state) => state.userLogin);
+	const navigate = useNavigate();
+	const { userInfo } = useSelector((state) => state.userLogin);
 
-    useEffect(() => {
-        if (!userInfo || !userInfo.isAdmin) {
-            navigate("/login");
-        }
-    }, [navigate, userInfo]);
+	useEffect(() => {
+		if (!userInfo || !userInfo.isAdmin) {
+			navigate("/login");
+		}
+	}, [navigate, userInfo]);
 
-    return children; // 如果通過檢查，渲染子元件
+	return children; // 如果通過檢查，渲染子元件
 };
 
 export default AdminRoute;
